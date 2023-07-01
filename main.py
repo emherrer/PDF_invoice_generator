f3162694aa902594e83c301f8317c182ff35c7f8
Emmanuel Herrera <emherrer1@gmail.com>
2023-06-30 23:13:21 -0400

    create minimal PDF for each excel file

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
 
 # Read excel files
 filepaths = glob.glob("invoices/*.xlsx")
 
+# Write each excel in PDF file
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
-    print(df)
+    
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{filepath[9:14]}", align="L", ln=1, border=0)
+    pdf.cell(w=50, h=8, txt=f"Date {filepath[15:24]}", align="L", ln=1, border=0)
+    pdf.output(f"PDF's/{filepath[9:24]}.pdf")
