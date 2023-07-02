0dcef1e2fadc681d06b8beb527de4d282dfca40c
Emmanuel Herrera <emherrer1@gmail.com>
2023-07-02 17:48:06 -0400

    add final config to the code

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ from fpdf import FPDF
 filepaths = glob.glob("invoices/*.xlsx")
 
 # Write each excel in PDF file
-for filepath in filepaths:   
+for filepath in filepaths:
     # Add PDF file for each excel file
     pdf = FPDF(orientation="P", unit="mm", format="A4")
-    
+
     # Extract info from excel files
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{filepath[9:14]}", align="L", ln=1, border=0)
-    pdf.cell(w=50, h=8, txt=f"Date {filepath[15:24]}", align="L", ln=1, border=0)
-    
+    pdf.cell(
+        w=50, h=8, txt=f"Invoice nr.{filepath[9:14]}", align="L", ln=1, border=0)
+    pdf.cell(
+        w=50, h=8, txt=f"Date {filepath[15:24]}", align="L", ln=1, border=0)
+
     # Read from excel
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     columns_name = df.columns
-    columns_name = [item.replace("_", " ").capitalize() for item in columns_name]
-    
+    columns_name = [item.replace("_", " ").capitalize()
+                    for item in columns_name]
+
     # Agregar nombre de columnas
     pdf.set_font(family="Times", style="B", size=10)
     pdf.cell(w=30, h=8, txt=str(columns_name[0]), ln=0, border=1)
@@ -29,7 +32,7 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=str(columns_name[3]), ln=0, border=1)
     pdf.cell(w=30, h=8, txt=str(columns_name[4]), ln=1, border=1)
 
-    # Agregar filas 
+    # Agregar filas
     for idx, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), ln=0, border=1)
@@ -46,13 +49,13 @@ for filepath in filepaths:
     pdf.cell(w=40, h=8, txt="", ln=0, border=1)
     pdf.cell(w=30, h=8, txt="", ln=0, border=1)
     pdf.cell(w=30, h=8, txt=str(total_price), ln=1, border=1)
-    
+
     # Agregar texto final
     pdf.set_font(family="Times", style="B", size=14)
-    pdf.cell(w=30, h=8, txt=f"The total price is: {total_price}", ln=1, border=0)
+    pdf.cell(
+        w=30, h=8, txt=f"The total price is: {total_price}", ln=1, border=0)
     pdf.cell(w=30, h=8, txt=f"PythonHow", ln=0, border=0)
     pdf.image("pythonhow.png", w=10)
 
-    
-    #Create PDF files
+    # Create PDF files
     pdf.output(f"PDF's/{filepath[9:24]}.pdf")
